b75cadd036dfbf153ddf01717c0918a21484648c
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-10 11:06:51 +0300

    Delete IAutoClose.java

--- src/main/java/org/example/IAutoClose.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import java.io.IOException;
-
-public interface IAutoClose extends AutoCloseable {
-    @Override
-    void close() throws IOException, AutoCloseException;
-}
