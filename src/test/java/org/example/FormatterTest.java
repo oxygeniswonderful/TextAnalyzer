44d0bac276fbb22059c4d2a3596afcf70268ba6d
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-10 08:20:52 +0300

    Update FormatterTest.java

--- src/test/java/org/example/FormatterTest.java
+++ src/test/java/org/example/FormatterTest.java
@@ -13,10 +13,31 @@ class FormatterTest {
 
     @Test
     void format() throws WriterException, ReaderException {
+
         String string = "    if (flex == rofl + chill ) { return KEK; }";
         String expected = "if (flex == rofl + chill ) {\n" +
                 "    return KEK;\n" +
                 "}\n";
+        init(string, expected);
+
+        String string1 = ";";
+        String expected1 = ";\n";
+        init(string1, expected1);
+
+        String string2 = "if(5!=2){";
+        String expected2 = "if(5!=2) {\n    ";
+        init(string2, expected2);
+
+        String string3 = " {";
+        String expected3 = " {\n    ";
+        init(string3, expected3);
+
+        String string4 = ";a";
+        String expected4 = ";\n";
+        init(string4, expected4);
+
+    }
+    void init(String string, String expected) throws WriterException, ReaderException {
         StringReaderImpl stringReader = new StringReaderImpl(string);
         StringBuilder stringBuilder = new StringBuilder();
         StringWriterImpl stringWriter = new StringWriterImpl(stringBuilder);
