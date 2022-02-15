395c9a735cd00d106650fdc822c46bc41a3b29a3
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-15 07:08:25 +0300

    Update Tools.java

--- src/main/java/org/example/Format/Tools.java
+++ src/main/java/org/example/Format/Tools.java
@@ -1,7 +1,6 @@
 package org.example.Format;
 
 public class Tools {
-
     public static String createIndent(int count) {
         StringBuilder indent = new StringBuilder();
         for (int i = 0; i < count; i++)
@@ -12,16 +11,14 @@ public class Tools {
     public static boolean isLeftBrace(String symbol) {
         return symbol.equals("{");
     }
-
     public static boolean isRightBrace(String symbol) {
         return symbol.equals("}");
     }
-
     public static boolean isSemicolon(String symbol) {
         return symbol.equals(";");
     }
-
+    public static boolean isShift(String symbol) { return symbol.equals("\n");}
     public static boolean isOrdinarySymbol(String symbol) {
-        return (Tools.isLeftBrace(symbol) | Tools.isRightBrace(symbol) | Tools.isSemicolon(symbol));
+        return (Tools.isLeftBrace(symbol) | Tools.isRightBrace(symbol) | Tools.isSemicolon(symbol) | Tools.isShift(symbol));
     }
 }
