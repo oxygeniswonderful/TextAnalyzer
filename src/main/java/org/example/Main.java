8beab97fb74dd9bc5fc94c4824a5b58ce5ad7677
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-17 10:01:04 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,36 @@
 package org.example;
 
 import org.example.Format.Formatter;
+import org.example.Format.Lexer;
 import org.example.Reader.FileReaderImpl;
 import org.example.Reader.ReaderException;
 import org.example.Writer.FileWriterImpl;
-import org.example.Writer.WriterException;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class Main {
-
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    
     public static void main(String[] args) throws Exception {
 
         try (
-            FileReaderImpl fileReader = new FileReaderImpl(new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8)));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(args[1], StandardCharsets.UTF_8));
-            FileWriterImpl fileWriter = new FileWriterImpl(bufferedWriter);
+                FileReaderImpl fileReader = new FileReaderImpl(new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8)));
+                FileWriterImpl fileWriter = new FileWriterImpl(new BufferedWriter(new FileWriter(args[1], StandardCharsets.UTF_8)));
         ) {
-            Formatter formatter = new Formatter(fileReader, fileWriter);
+
+            Lexer lexer = new Lexer(fileReader);
+            Formatter formatter = new Formatter(lexer, fileWriter);
             formatter.format();
-        } catch (ReaderException | WriterException e) {
-            System.err.println("Exception while read/write file: " + e);
+
+        } catch (ReaderException e) {
+            logger.error("Exception while read/write file: {}", e.toString());
         }
     }
 }
