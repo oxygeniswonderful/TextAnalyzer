b5c5cc9d0e136d2e4493a21b29341f3c9b993a9b
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-17 10:01:42 +0300

    Update FileReaderImpl.java

--- src/main/java/org/example/Reader/FileReaderImpl.java
+++ src/main/java/org/example/Reader/FileReaderImpl.java
@@ -1,30 +1,19 @@
 package org.example.Reader;
+import org.example.AutoClose.AutoCloseException;
+import org.example.AutoClose.IAutoClose;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import org.example.AutoClose.AutoCloseException;
-import org.example.AutoClose.IAutoClose;
 
 public class FileReaderImpl implements IReader, IAutoClose {
-
+    private static final Logger logger = LoggerFactory.getLogger(FileReaderImpl.class);
     private final BufferedReader bufferedReader;
     private int readedSymbol;
 
     public FileReaderImpl(BufferedReader bufferedReader) throws ReaderException {
         this.bufferedReader = bufferedReader;
-        readedSymbol = readSymbol();
-    }
-
-    @Override
-    public boolean hasChars() {
-        return readedSymbol != -1;
-    }
-
-    @Override
-    public char readChar() throws ReaderException {
-        char readedChar = (char) readedSymbol;
-        readedSymbol = readSymbol();
-        return readedChar;
     }
 
     @Override
@@ -33,15 +22,30 @@ public class FileReaderImpl implements IReader, IAutoClose {
             try {
                 bufferedReader.close();
             } catch (IOException e) {
+                logger.error("Can't close the file" + e);
                 throw new AutoCloseException("Can't close the file");
+
             }
         }
+    }
+
+    @Override
+    public boolean hasChars() {
+        return readedSymbol != -1;
+    }
+
+    @Override
+    public char readChars() throws ReaderException {
+        char readedChar = (char) readedSymbol;
+        readedSymbol = readSymbol();
+        return readedChar;
     }
 
     private int readSymbol() throws ReaderException {
         try {
             return bufferedReader.read();
         } catch (IOException e) {
+            logger.error(" Can't read symbol from the file" + e);
             throw new ReaderException(" Can't read symbol from the file");
         }
     }
