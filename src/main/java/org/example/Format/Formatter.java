86417d4228f7342062e1bedcc2e9cc9dc3beacdd
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-17 09:55:08 +0300

    Update Formatter.java

--- src/main/java/org/example/Format/Formatter.java
+++ src/main/java/org/example/Format/Formatter.java
@@ -1,104 +1,122 @@
 package org.example.Format;
-import org.example.Reader.IReader;
+import org.example.Token.IToken;
 import org.example.Reader.ReaderException;
 import org.example.Writer.IWriter;
 import org.example.Writer.WriterException;
-import java.util.Queue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Formatter implements IFormatter{
-
-    IReader reader;
+    private static final Logger logger = LoggerFactory.getLogger(Formatter.class);
+    ILexer lexer;
     IWriter writer;
 
-
-    public Formatter(IReader reader, IWriter writer) {
-        this.reader = reader;
+    public Formatter(ILexer lexer, IWriter writer) {
+        this.lexer = lexer;
         this.writer = writer;
     }
 
     @Override
     public void format() throws ReaderException, WriterException {
-        Lexer lexer = new Lexer(reader);
-        Queue<String> lexemes = lexer.analyse();
-        String symbol;
+
+        IToken token;
         String indent = "";
         String latestElement = "";
         int count = 0;
         int flag = 0;
 
+        while (lexer.hasMoreTokens()) {
+            token = lexer.readToken();
 
-        while (!lexemes.isEmpty()) {
-
-            symbol = lexemes.poll();
-
-            if (symbol.equals(" ") & (latestElement.equals(" ") | (latestElement.equals("")))) {
+            if (token.getLexeme() == null) {
                 continue;
             }
 
-            if (Tools.isOrdinarySymbol(latestElement) & symbol.equals(" ") & flag == 0) {
+            if (token.getLexeme().equals("\u0000")) {
                 continue;
             }
 
-            if (latestElement.equals("/") & symbol.equals("*")) {
+            if (token.getLexeme().equals(" ") & (latestElement.equals(" ") | (latestElement.equals("")))) {
+                continue;
+            }
+
+            if (Tools.isOrdinarySymbol(latestElement) & token.getLexeme().equals(" ") & flag == 0) {
+                continue;
+            }
+
+            if (latestElement.equals("/") & token.getLexeme().equals("*")) {
                 flag = 1;
             }
 
-            if (latestElement.equals("*") & symbol.equals("/")) {
-                writer.writeChar(symbol);
+            if (latestElement.equals("*") & token.getLexeme().equals("/")) {
+                logger.debug(token.getLexeme() + "\n");
+                logger.debug(indent);
+                writer.writeChar(token.getLexeme());
                 writer.writeChar("\n");
                 writer.writeChar(indent);
                 flag = 0;
                 continue;
             }
 
-            if (Tools.isShift(symbol)) {
-                writer.writeChar(symbol);
+            if (Tools.isShift(token.getLexeme())) {
+                writer.writeChar(token.getLexeme());
                 writer.writeChar(indent);
+                logger.debug(token.getLexeme());
+                logger.debug(indent);
             }
 
-            if (Tools.isLeftBrace(symbol) & !latestElement.equals(" ") & flag == 0) {
+            if (Tools.isLeftBrace(token.getLexeme()) & !latestElement.equals(" ") & flag == 0) {
                 writer.writeChar(" ");
+                logger.debug(" ");
             }
 
-            if (Tools.isSemicolon(latestElement) & !Tools.isRightBrace(symbol) & flag == 0) {
+            if (Tools.isSemicolon(latestElement) & !Tools.isRightBrace(token.getLexeme()) & flag == 0) {
                 writer.writeChar(indent);
+                logger.debug(indent);
             }
 
-            if(Tools.isRightBrace(latestElement) & !Tools.isOrdinarySymbol(symbol) & flag == 0) {
+            if(Tools.isRightBrace(latestElement) & !Tools.isOrdinarySymbol(token.getLexeme()) & flag == 0) {
                 writer.writeChar(indent);
+                logger.debug(indent);
             }
 
-            if (!Tools.isOrdinarySymbol(symbol)) {
-                writer.writeChar(symbol);
+            if (!Tools.isOrdinarySymbol(token.getLexeme())) {
+                writer.writeChar(token.getLexeme());
+                logger.debug(token.getLexeme());
             }
 
-            if (Tools.isLeftBrace(symbol) & flag == 0) {
+            if (Tools.isLeftBrace(token.getLexeme()) & flag == 0) {
                 count += 4;
                 indent = Tools.createIndent(count);
-                writer.writeChar(symbol);
+                writer.writeChar(token.getLexeme());
                 writer.writeChar("\n");
+                logger.debug(token.getLexeme() + "\n");
                 writer.writeChar(indent);
-               
+                logger.debug(indent);
             }
 
-            if ((Tools.isLeftBrace(symbol) | Tools.isRightBrace(symbol) | Tools.isSemicolon(symbol)) & flag == 1) {
-                writer.writeChar(symbol);
+            if ((Tools.isLeftBrace(token.getLexeme()) | Tools.isRightBrace(token.getLexeme()) | Tools.isSemicolon(token.getLexeme())) & flag == 1) {
+                writer.writeChar(token.getLexeme());
+                logger.debug(token.getLexeme());
             }
 
-            if (Tools.isRightBrace(symbol) & flag == 0) {
+            if (Tools.isRightBrace(token.getLexeme()) & flag == 0) {
                 count -= 4;
                 indent = Tools.createIndent(count);
                 writer.writeChar(indent);
-                writer.writeChar(symbol);
+                writer.writeChar(token.getLexeme());
+                logger.debug(indent);
+                logger.debug(token.getLexeme() + "\n");
                 writer.writeChar("\n");
 
             }
 
-            if (Tools.isSemicolon(symbol) & flag == 0) {
-                writer.writeChar(symbol);
+            if (Tools.isSemicolon(token.getLexeme()) & flag == 0) {
+                writer.writeChar(token.getLexeme());
+                logger.debug(token.getLexeme() + "\n");
                 writer.writeChar("\n");
             }
-            latestElement = symbol;
+            latestElement = token.getLexeme();
         }
     }
 }
