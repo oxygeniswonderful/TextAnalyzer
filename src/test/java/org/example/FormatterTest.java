8018c4121603bfbeba28b59e77ad71cb025aaef9
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-17 10:59:15 +0300

    Update FormatterTest.java

--- src/test/java/org/example/FormatterTest.java
+++ src/test/java/org/example/FormatterTest.java
@@ -1,13 +1,18 @@
 package org.example;
 
 import org.example.Format.Formatter;
+import org.example.Format.IFormatter;
+import org.example.Format.ILexer;
+import org.example.Format.Lexer;
 import org.example.Reader.ReaderException;
 import org.example.Reader.StringReaderImpl;
+import org.example.Token.TokenException;
 import org.example.Writer.StringWriterImpl;
 import org.example.Writer.WriterException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class FormatterTest {
 
@@ -35,13 +40,18 @@ class FormatterTest {
         String string4 = ";a";
         String expected4 = ";\n";
         init(string4, expected4);
-    }
 
+        String string5 = "/* for(int i = 0; i <= 10; i++) { */";
+        String expected5 = "/* for(int i = 0; i <= 10; i++) { */\n";
+        init(string5, expected5);
+
+    }
     void init(String string, String expected) throws WriterException, ReaderException {
         StringReaderImpl stringReader = new StringReaderImpl(string);
         StringBuilder stringBuilder = new StringBuilder();
         StringWriterImpl stringWriter = new StringWriterImpl(stringBuilder);
-        Formatter formatter = new Formatter(stringReader, stringWriter);
+        ILexer lexer = new Lexer(stringReader);
+        IFormatter formatter = new Formatter(lexer, stringWriter);
         formatter.format();
         assertEquals(stringBuilder.toString(), expected);
     }
