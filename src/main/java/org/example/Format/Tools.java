ae70eed2a89f2eafcba40bfeb0f5d6303fc65022
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-17 10:03:24 +0300

    Add files via upload

---
+++ src/main/java/org/example/Format/Tools.java
@@ -0,0 +1,32 @@
+package org.example.Format;
+
+public class Tools {
+    public static String createIndent(int count) {
+        StringBuilder indent = new StringBuilder();
+        for (int i = 0; i < count; i++)
+            indent.append(" ");
+        return indent.toString();
+    }
+
+    public static boolean isLeftBrace(String symbol) {
+        return symbol.equals("{");
+    }
+    public static boolean isRightBrace(String symbol) {
+        return symbol.equals("}");
+    }
+    public static boolean isSemicolon(String symbol) {
+        return symbol.equals(";");
+    }
+    public static boolean isShift(String symbol) {
+        return symbol.equals("\n");
+    }
+    public static boolean isBracket(Character symbol) {
+        return symbol.equals('(') | symbol.equals(')');
+    }
+    public static boolean isOperator(Character symbol) {
+        return symbol.equals('+') | symbol.equals('-') | symbol.equals('*') | symbol.equals('/') | symbol.equals('%') | symbol.equals('=');
+    }
+    public static boolean isOrdinarySymbol(String symbol) {
+        return (Tools.isLeftBrace(symbol) | Tools.isRightBrace(symbol) | Tools.isSemicolon(symbol) | Tools.isShift(symbol));
+    }
+}
