fc12923d989fb621842c52f14b16caa1f6a4ad83
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-02-17 11:18:13 +0300

    Update Formatter.java

--- src/main/java/org/example/Format/Formatter.java
+++ src/main/java/org/example/Format/Formatter.java
@@ -24,6 +24,7 @@ public class Formatter implements IFormatter{
         String latestElement = "";
         int count = 0;
         int flag = 0;
+        int isComment = 0;
 
         while (lexer.hasMoreTokens()) {
             token = lexer.readToken();
@@ -44,6 +45,20 @@ public class Formatter implements IFormatter{
                 continue;
             }
 
+            if (latestElement.equals("/") & token.getLexeme().equals("/")) {
+                isComment = 1;
+            }
+
+            if (Tools.isShift(token.getLexeme()) & isComment == 1) {
+                isComment = 0;
+                System.out.print(token.getLexeme());
+                writer.writeChar(token.getLexeme());
+                writer.writeChar(indent);
+                System.out.print(indent);
+                logger.debug(token.getLexeme());
+                logger.debug(indent);
+            }
+
             if (latestElement.equals("/") & token.getLexeme().equals("*")) {
                 flag = 1;
             }
@@ -51,6 +66,8 @@ public class Formatter implements IFormatter{
             if (latestElement.equals("*") & token.getLexeme().equals("/")) {
                 logger.debug(token.getLexeme() + "\n");
                 logger.debug(indent);
+                System.out.println(token.getLexeme());
+                System.out.print(indent);
                 writer.writeChar(token.getLexeme());
                 writer.writeChar("\n");
                 writer.writeChar(indent);
@@ -58,61 +75,64 @@ public class Formatter implements IFormatter{
                 continue;
             }
 
-            if (Tools.isShift(token.getLexeme())) {
-                writer.writeChar(token.getLexeme());
-                writer.writeChar(indent);
-                logger.debug(token.getLexeme());
-                logger.debug(indent);
-            }
-
             if (Tools.isLeftBrace(token.getLexeme()) & !latestElement.equals(" ") & flag == 0) {
                 writer.writeChar(" ");
+                System.out.print(" ");
                 logger.debug(" ");
             }
 
-            if (Tools.isSemicolon(latestElement) & !Tools.isRightBrace(token.getLexeme()) & flag == 0) {
+            if (Tools.isSemicolon(latestElement) & !Tools.isRightBrace(token.getLexeme()) & flag == 0 & isComment == 0) {
                 writer.writeChar(indent);
+                System.out.print(indent);
                 logger.debug(indent);
             }
 
-            if(Tools.isRightBrace(latestElement) & !Tools.isOrdinarySymbol(token.getLexeme()) & flag == 0) {
+            if(Tools.isRightBrace(latestElement) & !Tools.isOrdinarySymbol(token.getLexeme()) & flag == 0 & isComment == 0) {
                 writer.writeChar(indent);
+                System.out.print(indent);
                 logger.debug(indent);
             }
 
             if (!Tools.isOrdinarySymbol(token.getLexeme())) {
                 writer.writeChar(token.getLexeme());
+                System.out.print(token.getLexeme());
                 logger.debug(token.getLexeme());
             }
 
-            if (Tools.isLeftBrace(token.getLexeme()) & flag == 0) {
+            if (Tools.isLeftBrace(token.getLexeme()) & flag == 0 & isComment == 0) {
                 count += 4;
                 indent = Tools.createIndent(count);
                 writer.writeChar(token.getLexeme());
                 writer.writeChar("\n");
+                System.out.println(token.getLexeme());
                 logger.debug(token.getLexeme() + "\n");
                 writer.writeChar(indent);
+                System.out.print(indent);
                 logger.debug(indent);
             }
 
             if ((Tools.isLeftBrace(token.getLexeme()) | Tools.isRightBrace(token.getLexeme()) | Tools.isSemicolon(token.getLexeme())) & flag == 1) {
+                System.out.print(token.getLexeme());
                 writer.writeChar(token.getLexeme());
                 logger.debug(token.getLexeme());
             }
 
-            if (Tools.isRightBrace(token.getLexeme()) & flag == 0) {
+            if (Tools.isRightBrace(token.getLexeme()) & flag == 0 & isComment == 0) {
                 count -= 4;
                 indent = Tools.createIndent(count);
                 writer.writeChar(indent);
+                System.out.print(indent);
                 writer.writeChar(token.getLexeme());
+                System.out.println(token.getLexeme());
                 logger.debug(indent);
                 logger.debug(token.getLexeme() + "\n");
                 writer.writeChar("\n");
 
             }
 
-            if (Tools.isSemicolon(token.getLexeme()) & flag == 0) {
+            if (Tools.isSemicolon(token.getLexeme()) & flag == 0 & isComment == 0) {
                 writer.writeChar(token.getLexeme());
+                System.out.println(token.getLexeme());
                 logger.debug(token.getLexeme() + "\n");
                 writer.writeChar("\n");
             }
