3c782281a69bf5e646ea0e315ac12dc67cd58062
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-03-09 11:58:13 +0300

    Update IFormatter.java

--- src/main/java/org/example/Format/IFormatter.java
+++ src/main/java/org/example/Format/IFormatter.java
@@ -4,6 +4,7 @@ import org.example.Reader.ReaderException;
 import org.example.Writer.WriterException;
 
 public interface IFormatter {
+//void format() throws ReaderException, WriterException;
 
-    void format() throws ReaderException, WriterException;
+    void format(IToken token) throws ReaderException, WriterException;
 }
