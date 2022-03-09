b11bb27a69437d5d5e7557400b5b580abed2d435
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-03-09 13:46:02 +0300

    Update LexerContext.java

--- src/main/java/org/example/Format/LexerContext.java
+++ src/main/java/org/example/Format/LexerContext.java
@@ -2,18 +2,71 @@ package org.example.Format;
 
 import org.example.Token.IToken;
 import org.example.Token.TokenImpl;
+import org.example.Writer.IWriter;
+import org.example.Writer.WriterException;
 
 public class LexerContext {
     private String tokenName;
     private StringBuilder tokenLexeme;
     private StringBuilder buffer;
     private IToken token;
+    private IWriter writer;
+    private String latestElement;
+    private String indent;
+    private int count;
+    private int flag;
+    private int isComment;
+
+    public void setLatestElement(String latestElement) {
+        this.latestElement = latestElement;
+    }
+
+    public void setIndent(String indent) {
+        this.indent = indent;
+    }
+
+    public void setCount(int count) {
+        this.count += count;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public void setIsComment(int isComment) {
+        this.isComment = isComment;
+    }
 
     public LexerContext(IWriter writer) {
         this.tokenName = "";
         this.tokenLexeme = new StringBuilder();
         this.buffer = new StringBuilder();
         this.writer = writer;
+        this.latestElement = "";
+        this.indent = "";
+        this.count = 0;
+        this.flag = 0;
+        this.isComment = 0;
+    }
+
+    public String getLatestElement() {
+        return latestElement;
+    }
+
+    public String getLatestIndent() {
+        return indent;
+    }
+
+    public int getCountOfIndent() {
+        return count;
+    }
+
+    public int getFlagOfMultiComment() {
+        return flag;
+    }
+
+    public int getIsComment() {
+        return isComment;
     }
 
     public void WriteLexeme(String lexemeToWrite) {
