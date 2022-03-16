a5ad0bb10acb1e8d45c0370baf95883755c6d4a5
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2022-03-16 07:09:42 +0300

    Update TokenImpl.java

--- src/main/java/org/example/Token/TokenImpl.java
+++ src/main/java/org/example/Token/TokenImpl.java
@@ -1,18 +1,28 @@
 package org.example.Token;
 
+import java.util.Objects;
+
 public class TokenImpl implements IToken{
 
     private final String token;
     private final String lexeme;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TokenImpl)) return false;
+        TokenImpl token1 = (TokenImpl) o;
+        return Objects.equals(token, token1.token) && Objects.equals(lexeme, token1.lexeme);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, lexeme);
+    }
+
     public TokenImpl(String token, String lexeme) {
         this.token = token;
         this.lexeme = lexeme;
-    }
-
-    public TokenImpl(String lexeme) {
-        this.lexeme = lexeme;
-        this.token = String.valueOf(Token.EOF);
     }
 
     @Override
